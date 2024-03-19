1d93e04569d66e27ce77d56a6090325460fff6ce
juliaszczepanek <juliszczepanek12@gmail.com>
2024-03-19 15:39:29 +0100

    additional files
    adding files :>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">Little Lemon App
-    </div>
-  );
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  )
 }
+
+
+// console.log(rabbit);
+// let rabbit = 'Thumper';
+
 
 export default App;
