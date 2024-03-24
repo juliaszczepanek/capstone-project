8dcf231617322759033b4407faeeb0acc53a7e25
juliaszczepanek <juliszczepanek12@gmail.com>
2024-03-24 22:41:12 +0100

    repaired version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import './App.css';
-import React from 'react';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import Main from './components/Main';
-import Footer from './components/Footer';
 
-function App() {
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Nav, Footer } from './components';
+import { Header, Restaurant, Specials, Testimonial } from './components';
+
+import './App.css';
+
+import Reservation from './components/reservation/Reservation';
+import OrderOnline from './components/orderonline/OrderOnline';
+import Login from './components/login/Login';
+import About from './components/about/About';
+import Menu from './components/menu/Menu';
+
+const App = () => {
+
   return (
-    <>
-      <Header />
-      <Nav />
-      <Main />
-      <Footer />
-    </>
-  )
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/reservation" element={<Reservation />} />
+          <Route path="/orderonline" element={<OrderOnline />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-
-// console.log(rabbit);
-// let rabbit = 'Thumper';
-
+const Home = () => (
+  <div className="page">
+    <Header />
+    <Specials />
+    <Testimonial />
+    <Restaurant />
+  </div>
+);
 
 export default App;
